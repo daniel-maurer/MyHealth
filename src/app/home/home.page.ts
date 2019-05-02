f62c2618aa8f4fe2b1d9bfb773cc017220c02c48
Daniel Maurer <d.maurer@outlook.com.br>
2019-05-02 14:19:17 -0300

    Remove old home page code.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,12 @@
 import { MessagesPage } from './../messages/messages.page';
 import { Component, HostListener, ViewChild } from '@angular/core';
-import { NavController, NavParams, ModalController, PopoverController, IonContent } from '@ionic/angular';
+import {
+  NavController,
+  NavParams,
+  ModalController,
+  PopoverController,
+  IonContent
+} from '@ionic/angular';
 import { InformationPage } from './../information/information.page';
 import { NotificationsPage } from './../notifications/notifications.page';
 import { ToDosPage } from '../to-dos/to-dos.page';
@@ -17,16 +23,6 @@ export class HomePage {
 
   @ViewChild(IonContent) content: IonContent;
 
-  slideOpts2 = {
-    initialSlide: 1,
-    slidesPerView: 2,
-    //slidesPerView: 'auto',
-    spaceBetween: -20,
-    speed: 400,
-    centeredSlides: true
-    //  showPaginate:  false
-  };
-
   form = [
     { val: 'Caminhar 15 min', isChecked: true },
     { val: 'Medir glicose', isChecked: false },
@@ -36,30 +32,30 @@ export class HomePage {
   ];
 
   slideOpts = {
-  // Default parameters for smallest screen
-  slidesPerView: 1,
+    // Default parameters for smallest screen
+    slidesPerView: 1,
     spaceBetween: 10,
     centeredSlides: true,
-      // Responsive breakpoints
-      breakpointsInverse: true,
-        breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
+    // Responsive breakpoints
+    breakpointsInverse: true,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
         spaceBetween: -20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
         spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
         spaceBetween: 40
+      }
     }
-  }
-}
+  };
 
   constructor(
     public navCtrl: NavController,
@@ -76,16 +72,6 @@ export class HomePage {
     this.userService.mapObjectKey<User>(this.userService.currentUser).subscribe((user: User) => {
       this.currentUser = user;
     });
-  }
-
-  onResize(event) {
-    // let slidesPerView = Math.round($(window).width() / 110);
-    // if (slidesPerView < 3) {
-    //   slidesPerView = 3;
-    ///  }
-    ///  console.log(slidesPerView);
-    ///  this.slideOpts.slidesPerView = slidesPerView;
-    // swiper.onResize();
   }
 
   async onInformation() {
@@ -112,15 +98,15 @@ export class HomePage {
     return await popover.present();
   }
 
-  logScrollStart(){
-    console.log("logScrollStart : When Scroll Starts");
+  logScrollStart() {
+    console.log('logScrollStart : When Scroll Starts');
   }
 
-  logScrolling(){
-    console.log("logScrolling : When Scrolling");
+  logScrolling() {
+    console.log('logScrolling : When Scrolling');
   }
 
-  logScrollEnd(){
-    console.log("logScrollEnd : When Scroll Ends");
+  logScrollEnd() {
+    console.log('logScrollEnd : When Scroll Ends');
   }
 }
